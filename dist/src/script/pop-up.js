fcfc54d2759e08e886fd95fa79a6bcf8f00ef26e
MaureenDadap <maureendadap@gmail.com>
2020-02-24 21:26:08 +0800

    added pop-ups; to fix: duration of hide and show

---
+++ dist/src/script/pop-up.js
@@ -0,0 +1,78 @@
+var post;
+
+
+Handlebars.registerHelper("formatDate", function(date) {
+
+    var itemDate = new Date(date);
+
+    var string = itemDate.getDate() + "." + (itemDate.getMonth() + 1) + "." + itemDate.getFullYear() + " ";
+
+    if (itemDate.getHours() < 10) string = string + "0" + itemDate.getHours();
+    else string = string + itemDate.getHours();
+
+    string = string + ":";
+
+    if (itemDate.getMinutes() < 10) string = string + "0" + itemDate.getMinutes();
+    else string = string + itemDate.getMinutes();
+
+    return string;
+
+});
+
+Handlebars.registerHelper("parseHashtags", function(caption, type) {
+
+    caption = caption.replace(/\S*#(\[[^\]]+\]|\S+)/ig, '<a href=\'#\'>#$1</a>');
+
+    return new Handlebars.SafeString(caption);
+
+});
+
+Handlebars.registerHelper("linkUsername", function(username, type) {
+
+    switch (type) {
+
+        case 1:
+            return new Handlebars.SafeString("<a href='http://instagram.com/" + username + "' target='_blank'>" + username + "</a>");
+            break;
+
+        case 2:
+            return new Handlebars.SafeString("<a href='http://twitter.com/" + username + "' target='_blank'>" + username + "</a>");
+            break;
+
+    }
+
+});
+
+var cardTemplate = "<div class='pop-up card' id = 'popup' data-time='{{created_at}}'> " +
+    "{{#if image }}<div class='image'>" +
+    "<img src='{{image}}'>" +
+    "</div>{{/if}}" +
+    "<div class='content'>" +
+    "<div class='meta'>" +
+    "<span class='date'>{{formatDate created_at}}</span>" +
+    "</div>" +
+    "<div class='description'>" +
+    "{{parseHashtags caption type}}" +
+    "</div>" +
+    "</div>" +
+    "<div class='author type-{{type}}'>" +
+    "<img src='{{user.avatar}}' class='ui avatar image'>" +
+    "{{linkUsername user.username type}}" +
+    "</div>" +
+    "</div>";
+
+cardTemplate = Handlebars.compile(cardTemplate);
+
+$.getJSON(window.hashtagWallConfig.apiBaseUrl + "/posts", function(posts) {
+    post = posts[0];
+})
+
+var modal = document.querySelector(".modal");
+
+function toggleModal() {
+    $("#popup").remove();
+    $("#modal-content").append(cardTemplate(post));
+    modal.classList.toggle("show-modal");
+    //setTimeout(toggleModal, 3000);
+}
+setInterval(toggleModal, 15000);
